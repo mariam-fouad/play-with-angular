c76eb325e2ae0069f4bec6cc6c5bf9b2b6096b7f
Mariam <mariam.fouad94@gmail.com>
2019-01-17 23:18:25 +0300

    set up the orderManger data

--- app/scripts/services/ordermanger.js
+++ app/scripts/services/ordermanger.js
@@ -9,5 +9,53 @@
  */
 angular.module('udaciMealsApp')
   .service('orderManger', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+    let activeDay = "Monday";
+    let orderSelection = {
+      Monday:{
+        breakfast:'',
+        lunch:'',
+        dinner:''
+      },
+      Tuesday:{
+        breakfast:'',
+        lunch:'',
+        dinner:''
+      },
+      Wednesday:{
+        breakfast:'',
+        lunch:'',
+        dinner:''
+      },
+      Thursday:{
+        breakfast:'',
+        lunch:'',
+        dinner:''
+      },
+      Friday:{
+        breakfast:'',
+        lunch:'',
+        dinner:''
+      }
+    };
+
+  this.getActiveDay =()=>{
+    return activeDay;
+  };
+
+  this.setActiveDay = (day)=>{
+    this.activeDay=day;
+  };
+
+  this.getOrderSelection=()=>{
+    return orderSelection;
+  }
+
+  this.setOrderSelection = (meal , menuItem)=>{
+    orderSelection[activeDay][meal]=menuItem;
+  };
+
+  this.clearOrderSelection = (day,meal)=>{
+    orderSelection[day][meal]="";
+  };
+
   });
