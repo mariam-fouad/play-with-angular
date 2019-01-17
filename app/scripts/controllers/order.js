9245ca23c5acbc37e0bc11d74896bf0a346ef4ab
Mariam <mariam.fouad94@gmail.com>
2019-01-17 23:23:54 +0300

    create an order controller

---
+++ app/scripts/controllers/order.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsApp.controller:OrderCtrl
+ * @description
+ * # OrderCtrl
+ * Controller of the udaciMealsApp
+ */
+angular.module('udaciMealsApp')
+  .controller('OrderCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
