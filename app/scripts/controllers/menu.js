5902a5678cfeb7ed07acc5397cc2fe3fc95ea250
Mariam <mariam.fouad94@gmail.com>
2019-01-17 17:37:15 +0300

    show the list of items

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -9,25 +9,25 @@
  */
 angular.module('udaciMealsApp')
   .controller('MenuCtrl', function () {
-    this.items=[
+    this.items =[
       {
-        id="coconut-chocolate-icecream",
-        name="Coconut Chocolate Ice cream",
-        img="coconut-chocolate-icecream.jpg",
-        rating=4.9
+        id:"coconut-chocolate-icecream",
+        name:"Coconut Chocolate Ice cream",
+        img:"coconut-chocolate-icecream.jpg",
+        rating:4.9
       },
       {
-        id="bbq-chicken-ramen",
-        name="BBQ Chicken Ramen",
-        img="bbq-chicken-ramen.jpg",
-        rating=4.4
+        id:"bbq-chicken-ramen",
+        name:"BBQ Chicken Ramen",
+        img:"bbq-chicken-ramen.jpg",
+        rating:4.4
       },
       {
-        id="beef-steak-soft-tacos",
-        name="Beef Steak Soft Tacos",
-        img="beef-steak-soft-tacos.jpg",
-        rating=4.4
+        id:"beef-steak-soft-tacos",
+        name:"Beef Steak Soft Tacos",
+        img:"beef-steak-soft-tacos.jpg",
+        rating:3.9
       }
-    ]
+    ];
     
   });
