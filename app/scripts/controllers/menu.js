f34465f52d48d47d76a6d3b8aba749b629eff180
Mariam <mariam.fouad94@gmail.com>
2019-01-17 19:57:23 +0300

    use the foodFinder service

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -8,27 +8,14 @@
  * Controller of the udaciMealsApp
  */
 angular.module('udaciMealsApp')
-  .controller('MenuCtrl', function () {
-    this.items =[
-      {
-        id:"coconut-chocolate-icecream",
-        name:"Coconut Chocolate Ice cream",
-        img:"coconut-chocolate-icecream.jpg",
-        rating:4.9
-      },
-      {
-        id:"bbq-chicken-ramen",
-        name:"BBQ Chicken Ramen",
-        img:"bbq-chicken-ramen.jpg",
-        rating:4.4
-      },
-      {
-        id:"beef-steak-soft-tacos",
-        name:"Beef Steak Soft Tacos",
-        img:"beef-steak-soft-tacos.jpg",
-        rating:3.9
-      }
-    ];
+  .controller('MenuCtrl',['foodFinder' ,  function (menu) {
+    this.items = "";
+    
+    menu.getMenu().then((data)=>{
+      this.items=data;
+    })
+
+    
 
     this.incrementRating=(item)=>{
       item.rating = ((item.rating*10) + 1)/10;
@@ -44,4 +31,4 @@ angular.module('udaciMealsApp')
       }
     };
     
-  });
+  }]);
