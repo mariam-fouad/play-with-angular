42c29de8c1db8fe6af25f3209ebe6bfaa99e12da
Mariam <mariam.fouad94@gmail.com>
2019-01-18 17:30:48 +0300

    add a description to the menu

--- app/scripts/services/foodfinder.js
+++ app/scripts/services/foodfinder.js
@@ -14,19 +14,22 @@ angular.module('udaciMealsApp')
         "id":"coconut-chocolate-icecream",
         "name":"Coconut Chocolate Ice cream",
         "img":"coconut-chocolate-icecream.jpg",
-        "rating":4.9
+        "rating":4.9,
+        "description":'Smoth rich coconut ice cream with hot creamy choclate syrup'
       },
       {
         "id":"bbq-chicken-ramen",
         "name":"BBQ Chicken Ramen",
         "img":"bbq-chicken-ramen.jpg",
-        "rating":4.4
+        "rating":4.4,
+        "description":'hot fresh ramen noodels with tender BBQ chicken'
       },
       {
         "id":"beef-steak-soft-tacos",
         "name":"Beef Steak Soft Tacos",
         "img":"beef-steak-soft-tacos.jpg",
-        "rating":3.9
+        "rating":3.9,
+        "description":'strong flavour beef steak with soft tacos'
       }
     ]
     this.getMenu=()=>{
