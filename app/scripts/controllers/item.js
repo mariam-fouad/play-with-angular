3cc9542133338de7676fd0dd855e28810bfc79f1
Mariam <mariam.fouad94@gmail.com>
2019-01-18 17:33:27 +0300

    create a controller for the item

---
+++ app/scripts/controllers/item.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsApp.controller:ItemCtrl
+ * @description
+ * # ItemCtrl
+ * Controller of the udaciMealsApp
+ */
+angular.module('udaciMealsApp')
+  .controller('ItemCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
